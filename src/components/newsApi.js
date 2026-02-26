498e10c0f6913fa9116cecdc4fd43e8a9c9292a2
msislam-ai <148327900+msislam-ai@users.noreply.github.com>
2026-02-26 21:26:32 +0600

    Update newsApi.js

--- src/components/newsApi.js
+++ src/components/newsApi.js
@@ -1,6 +1,6 @@
 // src/api/newsApi.js
 
-const BASE_URL = "https://news-project-06582-2.onrender.com";
+const BASE_URL = "https://news-project-06582-2.onrender.com/news/all";
 
 /* =========================
    Fetch All News
